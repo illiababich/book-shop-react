0ef16592d97af202f0601fd27cce5c3918f34613
Illia <illia.babich.2@gmail.com>
2023-08-09 17:29:32 +0300

    disabling the user selection for Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,7 +52,7 @@ const Header: React.FC<HeaderProps> = () => {
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
           <Popover className="relative">
-            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 select-none">
               Books
               <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
             </Popover.Button>
@@ -71,7 +71,7 @@ const Header: React.FC<HeaderProps> = () => {
                   {products.map((item) => (
                     <div
                       key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50 select-none"
                     >
                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                         <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
@@ -81,7 +81,7 @@ const Header: React.FC<HeaderProps> = () => {
                           {item.name}
                           <span className="absolute inset-0" />
                         </a>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
+                        <p className="mt-1 text-gray-600 select-none">{item.description}</p>
                       </div>
                     </div>
                   ))}
@@ -90,13 +90,13 @@ const Header: React.FC<HeaderProps> = () => {
             </Transition>
           </Popover>
 
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 select-none">
             Best Sellers
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 select-none">
             New Arrivals
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 select-none">
             Special Offers
           </a>
         </Popover.Group>
