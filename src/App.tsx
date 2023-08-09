119485cba3625c36568bf826956b34e6ad563585
Illia <illia.babich.2@gmail.com>
2023-08-09 12:59:32 +0300

    starting to change the footer and creating the product card

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './index.css';
 import React from 'react';
 import Header from '../src/components/Header';
 import Footer from "./components/Footer";
+import BookCard from "./components/BookCard";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+        <BookCard name={"Harry Potter and the Sorcerer's Stone"} author={"J. K. Rowling"} price={14.99} image={""}/>
       <Footer />
     </div>
   );
