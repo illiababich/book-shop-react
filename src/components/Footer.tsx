a2abfee003a47f0a32c64b3485e3d70b4658a627
illiababich <illia.babich.2@gmail.com>
2023-08-10 23:02:51 +0300

    fixing footer overlapping, links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = () => {
     return (
-        <footer className="relative bg-blueGray-200 pt-8 pb-6">
-            <div className="container fixed inset-x-0 bottom-0 mx-auto px-4">
+        <footer className="relative bg-blueGray-200 pt-8 pb-6 mt-8">
+            <div className="container mx-auto px-4">
                 <div className="flex flex-wrap text-left lg:text-left">
                     <div className="w-full lg:w-6/12 px-4">
                         <h4 className="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
@@ -14,23 +14,25 @@ const Footer: React.FC<FooterProps> = () => {
                             Find us on any of these platforms, we respond 1-2 business days.
                         </h5>
                         <div className="mt-6 lg:mb-0 mb-6">
+                            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
                             <button
                                 className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                                 type="button">
                                 <i className="fab fa-twitter"></i></button>
+                            </a>
+                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
                             <button
                                 className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                                 type="button">
                                 <i className="fab fa-facebook-square"></i></button>
-                            <button
-                                className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                                type="button">
-                                <i className="fab fa-dribbble"></i></button>
+                            </a>
+                            <a href="https://github.com/illiababich" target="_blank" rel="noopener noreferrer">
                             <button
                                 className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                                 type="button">
                                 <i className="fab fa-github"></i>
                             </button>
+                            </a>
                         </div>
                     </div>
                     <div className="w-full lg:w-6/12 px-4">
@@ -38,34 +40,6 @@ const Footer: React.FC<FooterProps> = () => {
                             <div className="w-full lg:w-4/12 px-4 ml-auto">
                                 <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
                                 <ul className="list-unstyled">
-                                    <li>
-                                        <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                                           href="">About
-                                            Us</a>
-                                    </li>
-                                    <li>
-                                        <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                                           href="">Blog</a>
-                                    </li>
-                                    <li>
-                                        <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                                           href="">Github</a>
-                                    </li>
-                                    <li>
-                                        <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                                           href="">Free
-                                            Products</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="w-full lg:w-4/12 px-4">
-                                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
-                                <ul className="list-unstyled">
-                                    <li>
-                                        <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                                           href="">MIT
-                                            License</a>
-                                    </li>
                                     <li>
                                         <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                                            href="">Terms &amp; Conditions</a>
